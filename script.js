f2f115ddac8b0e6f38a5e68158bbf3e4e46dfdaa
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-11 21:55:48 -0300

    Adicionar funcionalidades de senha e botao de ver senha

---
+++ script.js
@@ -0,0 +1,46 @@
+const senhas = document.querySelectorAll('input[type=password]');
+const botaoVerSenhas = document.querySelector('button.senhas');
+
+const validarSenha = () => {
+  if (senhas[0].value && senhas[1].value) {
+    if (senhas[0].value !== senhas[1].value) {
+      document.querySelector('.erro-senha').style.visibility = 'visible';
+
+      for (const input of senhas) {
+        input.style.outline = '1px solid red';
+      }
+    }
+  }
+}
+
+const resetSenha = () => {
+  document.querySelector('.erro-senha').style.visibility = '';
+
+  for (const input of senhas) {
+    input.style.outline = '';
+  }
+}
+
+const mostrarSenhas = event => {
+  for (const input of senhas) {
+    input.type = 'text';
+  }
+
+  event.target.style.backgroundColor = 'rgba(137, 43, 226, 0.35)';
+}
+
+const esconderSenhas = () => {
+  for (const input of senhas) {
+    input.type = 'password';
+  }
+
+  event.target.style.backgroundColor = '';
+}
+
+for (const input of senhas) {
+  input.onblur = validarSenha;
+  input.onchange = resetSenha;
+}
+
+botaoVerSenhas.onmousedown = mostrarSenhas;
+botaoVerSenhas.onmouseup = esconderSenhas;
